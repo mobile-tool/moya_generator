9093bc0c16f039ee2c3c994b705e2c22e513452f
bany <bany.y0599@gmail.com>
2022-03-12 15:00:11 +0700

    moya_generator: add pare curl

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 const yargs = require("yargs");
-const dotenv = require( "dotenv" );
+const dotenv = require( "dotenv");
+const parse = require('parse-curl');
 const info = require('./info');
 
 dotenv.config();
 
 info();
+
 const options = yargs
- .usage("Usage: -n <name>")
- .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
- .argv;
+    .usage("Usage: -c <curl>")
+    .option("c", { alias: "curl", describe: "Your curl", type: "string", demandOption: true })
+    .argv;
+
+const out = parse(options.c);
+
+console.log(JSON.stringify(out, null, 2))
