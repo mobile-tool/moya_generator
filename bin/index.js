bf2f0a97e93d5d01c86dc6376c39e84de908740d
bany <bany.y0599@gmail.com>
2022-03-12 13:06:59 +0700

    moya_generator: clean name module

--- bin/index.js
+++ bin/index.js
@@ -1,27 +1,12 @@
 #!/usr/bin/env node
-const chalk = require("chalk");
-const boxen = require("boxen");
 const yargs = require("yargs");
+const dotenv = require( "dotenv" );
+const info = require('./info');
 
+dotenv.config();
+
+info();
 const options = yargs
  .usage("Usage: -n <name>")
  .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
  .argv;
-
- const greetingString = `Hello, ${options.name}!`;
-
-const greeting = chalk.white.bold(`Hello ${greetingString} !`);
-
-const boxenOptions = {
- padding: 1,
- margin: 1,
- borderStyle: "round",
- borderColor: "green",
- backgroundColor: "#555555"
-};
-const msgBox = boxen( greeting, boxenOptions );
-
-
-
-
-console.log(msgBox);
