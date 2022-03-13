94aec39a7e2858624740474986bae57d2250971d
bany <bany.y0599@gmail.com>
2022-03-13 09:23:55 +0700

    moya_generator: remove pare-json

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const yargs = require("yargs");
 const dotenv = require( "dotenv");
-const parse = require('parse-curl');
+const parse = require('./pare-json');
 const info = require('./info');
 
 dotenv.config();
@@ -15,4 +15,48 @@ const options = yargs
 
 const out = parse(options.c);
 
+// let url = new URL(out.url);
+
+// const pareOutput = `
+// public enum GitHub {
+//     case zen
+// }
+
+// extension GitHub: TargetType {
+//     public var baseURL: URL { URL(string: "${out.url}")! }
+//     public var path: String {
+//         switch self {
+//         case .zen:
+//             return "/zen"
+//         }
+//     }
+
+//     public var method: Moya.Method { .get }
+
+//     public var task: Task {
+//         switch self {
+//         case .userRepositories:
+//             return .requestParameters(parameters: ["sort": "pushed"], encoding: URLEncoding.default)
+//         default:
+//             return .requestPlain
+//         }
+//     }
+//     public var validationType: ValidationType {
+//         switch self {
+//         case .zen:
+//             return .successCodes
+//         default:
+//             return .none
+//         }
+//     }
+//     public var sampleData: Data {
+//         return Data()
+//     }
+//     public var headers: [String: String]? { nil }
+
+// }
+// `;
+
 console.log(JSON.stringify(out, null, 2))
+
+// console.log(pareOutput);
